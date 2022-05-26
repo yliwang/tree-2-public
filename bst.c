ef4f8080daa1bb67faeb426a54a352e70811f604
yliwang <1220264221@qq.com>
2022-05-26 17:41:26 +0800

    Update bst.c

--- bst.c
+++ bst.c
@@ -9,40 +9,6 @@
 void is_bst_recursive(struct node *r, int *flag )
 {
 	assert(r != NULL);
-	T a, b, c;
-
-	if (*flag) return;
-
-	if (r->left && r->right ) {
-		a = r->left->element;
-		b = r->element;
-		c = r->right->element;
-		if( a > b || c > b ) {
-			*flag += 1;
-			return; /* not need to check any more*/
-		}
-	}
-	
-	if (r->left) {
-		a = r->left->element;
-		b = r->element;
-		if (a > b) {
-			*flag += 1;
-			return;
-		}
-	}
-	
-	if (r->right) {
-		b = r->element;
-		c = r->right->element;
-		if (b > c) {
-			*flag += 1;
-			return;
-		}
-	}
-
-	if (r->left) is_bst_recursive(r->left, flag);
-	if (r->right) is_bst_recursive(r->right, flag);
 }
 
 
